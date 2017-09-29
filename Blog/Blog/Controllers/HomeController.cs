dd07e239e350a407fba4011b308b907bf38766b1
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2017-09-29 09:30:20 -0300

    Adiciona exs 1.8 - Views dinamicas

--- Blog/Blog/Controllers/HomeController.cs
+++ Blog/Blog/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,14 @@ namespace Blog.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            var listaDePosts = new List<Post>()
+            {
+                new Post { Titulo = "Harry potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
+                new Post { Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" },
+                new Post { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" },
+                new Post { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" }
+            };
+            ViewBag.Posts = listaDePosts;
             return View();
         }
     }
