baecbad56559eb3cce8619e827a0bc84b64b780d
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2017-09-29 11:18:15 -0300

    Adiciona exs 1.14 - Adicao de Post com HttpPost

--- Blog/Blog/Controllers/HomeController.cs
+++ Blog/Blog/Controllers/HomeController.cs
@@ -33,6 +33,7 @@ namespace Blog.Controllers
             return View();
         }
 
+        [HttpPost]
         public ActionResult AdicionaPost(Post post)
         {
             lista.Add(post);
