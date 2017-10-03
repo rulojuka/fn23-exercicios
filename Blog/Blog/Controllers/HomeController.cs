dff3acd203e2a762250665f799730d8a01a6291b
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2017-10-03 18:42:06 -0300

    Adiciona PostDAO

--- Blog/Blog/Controllers/HomeController.cs
+++ Blog/Blog/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Infra;
+﻿using Blog.DAO;
+using Blog.Infra;
 using Blog.Models;
 using System;
 using System.Collections.Generic;
@@ -12,29 +13,17 @@ namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
+        PostDAO postDAO;
+
+        public HomeController()
+        {
+            postDAO = new PostDAO();
+        }
 
         // GET: Home
         public ActionResult Index()
         {
-            var lista = new List<Post>();
-            using (SqlConnection cnx = ConnectionFactory.CriaConexaoAberta())
-            {
-                SqlCommand comando = cnx.CreateCommand();
-                comando.CommandText = "select * from Posts";
-                SqlDataReader leitor = comando.ExecuteReader();
-                while (leitor.Read())
-                {
-                    Post post = new Post()
-                    {
-                        Id = Convert.ToInt32(leitor["id"]),
-                        Titulo = Convert.ToString(leitor["titulo"]),
-                        Resumo = Convert.ToString(leitor["resumo"]),
-                        Categoria = Convert.ToString(leitor["categoria"])
-                    };
-                    lista.Add(post);
-                }
-            }
-            return View(lista);
+            return View(postDAO.Lista());
         }
 
         public ActionResult NovoPost()
@@ -45,16 +34,7 @@ namespace Blog.Controllers
         [HttpPost]
         public ActionResult AdicionaPost(Post post)
         {
-            string stringConexao = ConfigurationManager.ConnectionStrings["blog"].ConnectionString;
-            using (SqlConnection cnx = ConnectionFactory.CriaConexaoAberta())
-            {
-                SqlCommand comando = cnx.CreateCommand();
-                comando.CommandText = "insert into Posts (Titulo, Resumo, Categoria) values (@titulo, @resumo, @categoria)";
-                comando.Parameters.Add(new SqlParameter("Titulo", post.Titulo));
-                comando.Parameters.Add(new SqlParameter("Resumo", post.Resumo));
-                comando.Parameters.Add(new SqlParameter("Categoria", post.Categoria));
-                comando.ExecuteNonQuery();
-            }
+            postDAO.Adiciona(post);
             return RedirectToAction("Index");
         }
     }
---
+++ Blog/Blog/DAO/PostDAO.cs
@@ -0,0 +1,52 @@
+﻿using Blog.Infra;
+using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Blog.DAO
+{
+    public class PostDAO
+    {
+        public void Adiciona(Post post)
+        {
+            string stringConexao = ConfigurationManager.ConnectionStrings["blog"].ConnectionString;
+            using (SqlConnection cnx = ConnectionFactory.CriaConexaoAberta())
+            {
+                SqlCommand comando = cnx.CreateCommand();
+                comando.CommandText = "insert into Posts (Titulo, Resumo, Categoria) values (@titulo, @resumo, @categoria)";
+                comando.Parameters.Add(new SqlParameter("Titulo", post.Titulo));
+                comando.Parameters.Add(new SqlParameter("Resumo", post.Resumo));
+                comando.Parameters.Add(new SqlParameter("Categoria", post.Categoria));
+                comando.ExecuteNonQuery();
+            }
+        }
+
+        public IList<Post> Lista()
+        {
+            var lista = new List<Post>();
+            using (SqlConnection cnx = ConnectionFactory.CriaConexaoAberta())
+            {
+                SqlCommand comando = cnx.CreateCommand();
+                comando.CommandText = "select * from Posts";
+                SqlDataReader leitor = comando.ExecuteReader();
+                while (leitor.Read())
+                {
+                    Post post = new Post()
+                    {
+                        Id = Convert.ToInt32(leitor["id"]),
+                        Titulo = Convert.ToString(leitor["titulo"]),
+                        Resumo = Convert.ToString(leitor["resumo"]),
+                        Categoria = Convert.ToString(leitor["categoria"])
+                    };
+                    lista.Add(post);
+                }
+            }
+
+            return lista;
+        }
+    }
+}
