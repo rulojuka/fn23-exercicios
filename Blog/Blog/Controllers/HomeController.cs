35e2c92fe8eb1b35787976ead0312f001d386922
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2017-10-03 17:22:25 -0300

    Adiciona exs 3.4 e 3.6 Adiciona banco ao projeto e lista posts a partir dele

--- Blog/Blog/Controllers/HomeController.cs
+++ Blog/Blog/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Blog.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,22 +11,30 @@ namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
-        private IList<Post> lista;
-
-        public HomeController()
-        {
-            this.lista = new List<Post>()
-            {
-                new Post { Titulo = "Harry potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
-                new Post { Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" },
-                new Post { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" },
-                new Post { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" }
-            };
-        }
 
         // GET: Home
         public ActionResult Index()
         {
+            var lista = new List<Post>();
+            string stringConexao = ConfigurationManager.ConnectionStrings["blog"].ConnectionString;
+            using (SqlConnection cnx = new SqlConnection(stringConexao))
+            {
+                cnx.Open();
+                SqlCommand comando = cnx.CreateCommand();
+                comando.CommandText = "select * from Posts";
+                SqlDataReader leitor = comando.ExecuteReader();
+                while (leitor.Read())
+                {
+                    Post post = new Post()
+                    {
+                        Id = Convert.ToInt32(leitor["id"]),
+                        Titulo = Convert.ToString(leitor["titulo"]),
+                        Resumo = Convert.ToString(leitor["resumo"]),
+                        Categoria = Convert.ToString(leitor["categoria"])
+                    };
+                    lista.Add(post);
+                }
+            }
             return View(lista);
         }
 
@@ -36,8 +46,8 @@ namespace Blog.Controllers
         [HttpPost]
         public ActionResult AdicionaPost(Post post)
         {
-            lista.Add(post);
-            return View("Index", lista);
+            //lista.Add(post);
+            return View(/*"Index", lista*/);
         }
     }
 }
