b4fbd91bcc9c45816fcead34049e0babf0336ce7
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2017-10-12 06:38:01 -0300

    Adiciona exs 9.5 - Tela e ViewModel de Login

---
+++ Blog/Blog/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Post", new { area = "Admin" });
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
---
+++ Blog/Blog/Models/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Usuário")]
+        public string LoginName { get; set; }
+        [Required]
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
