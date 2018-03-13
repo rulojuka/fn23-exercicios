a2e81f4f270c4921e5ee50f0c6eff80b8ae57df6
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2018-03-13 19:13:49 -0300

    Adiciona exs de criacao do banco

---
+++ Blog/Blog/Infra/ConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Blog.Infra
+{
+    public class ConnectionFactory
+    {
+        public static SqlConnection CriaConexaoAberta()
+        {
+            string stringConexao = ConfigurationManager.ConnectionStrings["blog"].ConnectionString;
+            SqlConnection conexao = new SqlConnection(stringConexao);
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
