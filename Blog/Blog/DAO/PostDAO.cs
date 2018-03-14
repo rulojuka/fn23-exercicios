3cdfe9e5defa64edf38c38782a43cf35a95ae5c3
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2018-03-13 21:09:15 -0300

    Adiciona Entity Framework e faz listagem e adicao de posts com ele

--- Blog/Blog/DAO/PostDAO.cs
+++ Blog/Blog/DAO/PostDAO.cs
@@ -1,8 +1,8 @@
 ﻿using Blog.Infra;
 using Blog.Models;
-using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace Blog.DAO
 {
@@ -10,37 +10,19 @@ namespace Blog.DAO
     {
         public IList<Post> Lista()
         {
-            var lista = new List<Post>();
-            using (SqlConnection cnx = ConnectionFactory.CriaConexaoAberta())
+            using (BlogContext contexto = new BlogContext())
             {
-                SqlCommand comando = cnx.CreateCommand();
-                comando.CommandText = "select * from Posts";
-                SqlDataReader leitor = comando.ExecuteReader();
-                while (leitor.Read())
-                {
-                    Post post = new Post()
-                    {
-                        Id = Convert.ToInt32(leitor["id"]),
-                        Titulo = Convert.ToString(leitor["titulo"]),
-                        Resumo = Convert.ToString(leitor["resumo"]),
-                        Categoria = Convert.ToString(leitor["categoria"])
-                    };
-                    lista.Add(post);
-                }
+                var lista = contexto.Posts.ToList();
+                return lista;
             }
-            return lista;
         }
 
         public void Adiciona(Post post)
         {
-            using (SqlConnection cnx = ConnectionFactory.CriaConexaoAberta())
+            using (BlogContext contexto = new BlogContext())
             {
-                SqlCommand comando = cnx.CreateCommand();
-                comando.CommandText = "insert into Posts (Titulo, Resumo, Categoria) values (@titulo, @resumo, @categoria)";
-                comando.Parameters.Add(new SqlParameter("titulo", post.Titulo));
-                comando.Parameters.Add(new SqlParameter("resumo", post.Resumo));
-                comando.Parameters.Add(new SqlParameter("categoria", post.Categoria));
-                comando.ExecuteNonQuery();
+                contexto.Posts.Add(post);
+                contexto.SaveChanges();
             }
         }
     }
---
+++ Blog/Blog/Infra/BlogContext.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Models;
+using System.Data.Entity;
+
+namespace Blog.Infra
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext() : base("name=blog")
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
